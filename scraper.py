8295c6ce255569ef86b819e9b4e3200089b2f0c9
ebenp <eben.pendleton@gmail.com>
2017-11-17 07:40:19 -0500

    Update PhantomJS

--- scraper.py
+++ scraper.py
@@ -1,21 +1,27 @@
-import scraperwiki
-from splinter import Browser
-#import requests
-#import pandas as pd
-#from bs4 import BeautifulSoup
+#!/usr/bin/env python
+"""Provides scraping functionality for commercial landings page. Uses headless PhantomJS
+"""
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support import expected_conditions as expected
+from selenium.webdriver.support.wait import WebDriverWait
+# html table parser modified from github
+from htmltableparser import HTMLTableParser
 
-def open_url_query_years(browser):
+def open_url_query_years(wait):
     '''
     Queries given url by qyear. Returns years
     :param wait: wait from WebDriverWait
     :return: list of queryable years
     '''
     # get our selector options
-    select = browser.find_by_id("qyear")
+    select = Select(wait.until(expected.visibility_of_element_located((By.ID,"qyear"))))
 
-    return select.text.split('\n')
+    return [o.text for o in select.options]
 
-def parse_table(browser,url,year):
+def parse_table(wait,driver,url,year):
 
     '''
     Parses a queried page to an HTML and dataframe
@@ -26,43 +32,59 @@ def parse_table(browser,url,year):
     :return: html, html source and table as a pandas dataframe
     '''
 
-    browser.visit(url)
-    browser.find_by_xpath("//select[@name='qyear']/option[text()=" + year + "]").click()
-    browser.find_by_xpath("//input[@value='Submit Query'][@type='submit']").click()
-    html = browser.html
-    '''
+    driver.get(url)
+    wait.until(expected.visibility_of_element_located((By.XPATH,"//select[@name='qyear']/option[text()=" + year + "]"))).click()
+    wait.until(expected.visibility_of_element_located((By.XPATH, "//input[@value='Submit Query'][@type='submit']"))).click()
+    html = driver.page_source
     hp = HTMLTableParser()
     table = hp.parse_url(url, html=html, singletable=True)[0][1]  # Grabbing the table from the tuple
-    '''
-    return html
-'''
-Main script with user inputs
-'''
 
-# inputs
+    return html, table
+
+def save_html_text(html,df,outpath,year):
+
+    '''
+    Saves table to html and text files.
+
+    :param html: html source as string to save
+    :param df: dataframe of able to save
+    :param outpath: path to directory to save html and text files. The year is appended here
+    :param year: year label for file names
+    :return: None
+    '''
+
+    df.to_csv(outpath+r'//'+year+'.txt', sep='\t',index=False)
+    with open(outpath+r'//'+year+'.html', "w") as file:
+        file.write(html)
+    return
 
 if __name__ == '__main__':
 
+    '''
+    Main script with user inputs
+    '''
+
+    # inputs
     url = 'https://www.st.nmfs.noaa.gov/commercial-fisheries/commercial-landings' + \
           '/other-specialized-programs' + \
           '/total-commercial-fishery-landings-at-major-u-s-ports-summarized-by-year-and-ranked-by-dollar-value/index'
+    output_dir='/Users/eben/Documents/GitHub/scraping_landings_data/text_output'
+
     # processing
     # set up our driver here
-    with Browser() as browser:
-        # access the url with the driver
-        browser.visit(url)
-        # obtain years to access tables
-        years = open_url_query_years(browser)
-        # obtain and save tables in html and tab delimited text for each year desired
-        for year in years:
-            print(year)
-            html = parse_table(browser, url, year)
-            # Write out to the sqlite database using scraperwiki library
-            scraperwiki.sqlite.save(unique_keys=['year'], data={"year": year, "html": html})
+    options = Options()
+    driver = webdriver.PhantomJS()
+    # set up wait
+    wait = WebDriverWait(driver, timeout=10)
+    # access the url with the driver
+    driver.get(url)
+    # obtain years to acess tables
+    years = open_url_query_years(wait)
+    # obtain and save tables in html and tab delimited text for each year desired
+    for year in years:
+        print(year)
+        html, df = parse_table(wait, driver, url, year)
+        save_html_text(html, df, output_dir,year)
 
-
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+    # Print completion
+    print('DONE!')
