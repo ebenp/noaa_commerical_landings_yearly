d10d0e899f9f4c8b4054e361786fd5ecb11b5f8a
ebenp <eben.pendleton@gmail.com>
2017-11-19 14:39:54 -0500

    Added error handling

--- scraper.py
+++ scraper.py
@@ -1,4 +1,7 @@
 from splinter import Browser
+import sys, shutil
+
+dev_mode = True
 
 with Browser("phantomjs") as browser:
     # Optional, but make sure large enough that responsive pages don't
@@ -8,10 +11,17 @@ with Browser("phantomjs") as browser:
     # Open the page you want...
     browser.visit("https://morph.io")
 
+    if dev_mode:
+        browser.screenshot('screen_0001.png')  # save a screenshot to disk
+
     # submit the search form...
     browser.fill("q", "parliament")
     button = browser.find_by_css("button[type='submit']")
     button.click()
+
+    if dev_mode is True:
+        with open("ghostdriver.log", "r") as f:
+            shutil.copyfileobj(f, sys.stdout)
 
     # Scrape the data you like...
     links = browser.find_by_css(".search-results .list-group-item")
