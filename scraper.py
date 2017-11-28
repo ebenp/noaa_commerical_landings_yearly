75e71593d3b4049bc7308ef46422a164573cfaec
ebenp <eben.pendleton@gmail.com>
2017-11-27 22:20:03 -0500

    updated scraper to have phantomjs executable

--- scraper.py
+++ scraper.py
@@ -1,28 +1,93 @@
-from splinter import Browser
-import sys, shutil
+#!/usr/bin/env python
+"""Provides scraping functionality for commercial landings page. Uses headless PhantomJS
+"""
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support import expected_conditions as expected
+from selenium.webdriver.support.wait import WebDriverWait
+# html table parser modified from github
+from htmltableparser import HTMLTableParser
 
-dev_mode = False
-with Browser("phantomjs", service_args=['--ignore-ssl-errors=true', '--ssl-protocol=any']) as browser:
-    # Optional, but make sure large enough that responsive pages don't
-    # hide elements on you...
-    browser.driver.set_window_size(1280, 1024)
+def open_url_query_years(wait):
+    '''
+    Queries given url by qyear. Returns years
+    :param wait: wait from WebDriverWait
+    :return: list of queryable years
+    '''
+    # get our selector options
+    select = Select(wait.until(expected.visibility_of_element_located((By.ID,"qyear"))))
 
-    # Open the page you want...
-    browser.visit("https://morph.io")
+    return [o.text for o in select.options]
 
-    if dev_mode:
-        browser.screenshot('screen_0001.png')  # save a screenshot to disk
+def parse_table(wait,driver,url,year):
 
-    # submit the search form...
-    browser.fill("q", "parliament")
-    button = browser.find_by_css("button[type='submit']")
-    button.click()
+    '''
+    Parses a queried page to an HTML and dataframe
+    :param wait: wait from WebDriverWait
+    :param driver: Firefox Driver
+    :param url: input url to query
+    :param year: year to select
+    :return: html, html source and table as a pandas dataframe
+    '''
 
-    if dev_mode is True:
-        with open("ghostdriver.log", "r") as f:
-            shutil.copyfileobj(f, sys.stdout)
+    driver.get(url)
+    wait.until(expected.visibility_of_element_located((By.XPATH,"//select[@name='qyear']/option[text()=" + year + "]"))).click()
+    wait.until(expected.visibility_of_element_located((By.XPATH, "//input[@value='Submit Query'][@type='submit']"))).click()
+    html = driver.page_source
+    hp = HTMLTableParser()
+    table = hp.parse_url(url, html=html, singletable=True)[0][1]  # Grabbing the table from the tuple
 
-    # Scrape the data you like...
-    links = browser.find_by_css(".search-results .list-group-item")
-    for link in links:
-        print(link['href'])
+    return html, table
+
+def save_html_text(html,df,outpath,year):
+
+    '''
+    Saves table to html and text files.
+
+    :param html: html source as string to save
+    :param df: dataframe of able to save
+    :param outpath: path to directory to save html and text files. The year is appended here
+    :param year: year label for file names
+    :return: None
+    '''
+
+    df.to_csv(outpath+r'//'+year+'.txt', sep='\t',index=False)
+    with open(outpath+r'//'+year+'.html', "w") as file:
+        file.write(html)
+    return
+
+if __name__ == '__main__':
+
+    '''
+    Main script with user inputs
+    '''
+
+    # inputs
+    url = 'https://www.st.nmfs.noaa.gov/commercial-fisheries/commercial-landings' + \
+          '/other-specialized-programs' + \
+          '/total-commercial-fishery-landings-at-major-u-s-ports-summarized-by-year-and-ranked-by-dollar-value/index'
+    output_dir='/Users/eben/Documents/GitHub/scraping_landings_data/text_output'
+
+    # processing
+    # set up our driver here
+    options = Options()
+    # Morph.io exacutable path
+    #executable_path='/usr/bin/phantomjs'
+    driver = webdriver.PhantomJS(service_args=['--web-security=no',
+                                               '--ssl-protocol=any', '--ignore-ssl-errors=yes'])
+    # set up wait
+    wait = WebDriverWait(driver, timeout=10)
+    # access the url with the driver
+    driver.get(url)
+    # obtain years to acess tables
+    years = open_url_query_years(wait)
+    # obtain and save tables in html and tab delimited text for each year desired
+    for year in years:
+        print(year)
+        html, df = parse_table(wait, driver, url, year)
+        save_html_text(html, df, output_dir,year)
+
+    # Print completion
+    print('DONE!')
